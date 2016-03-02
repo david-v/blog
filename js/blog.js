08b93f3c10a885ddd1d02ba42a428e61c9f17a1d
David Velilla <david@neonplay.com>
2016-03-02 12:03:32 +0000

    Set state of form to 'pristine' upon success posting

--- js/blog.js
+++ js/blog.js
@@ -30,108 +30,108 @@
             '$http', '$sanitize', '$cookies', '$timeout', 
     function($http,   $sanitize,   $cookies,   $timeout) {
     
-    var blog = this;
-    blog.title = "david.veli!la";
-    blog.captcha = 8;
-    blog.date = new Date();
-    blog.posts = [];
-    blog.allTags = [];
-    blog.selectedTags = [];
-
-    $http({
-      method: 'GET',
-      url: 'http://localhost:8000/blog/posts'
-    }).then(
-      function(response){
-        console.log($cookies.get('csrftoken'));
-        $http.defaults.headers.post['X-CSRFToken'] = $cookies.get('csrftoken');
-
-        allPostsData = response.data;
-        for (var i = 0; i < allPostsData.length; i++) {
-          var postData = allPostsData[i];
-          var post = new Post(postData);
-          for (var j = 0; j < post.tags.length; j++) {
-            var tag = post.tags[j];
-            if (-1 == blog.allTags.indexOf(tag)) {
-              blog.allTags.push(tag);
-            }
-          }
-          blog.posts.push(post);
-        }
-        blog.posts[0].toggle();
-        $timeout(onDomLoaded, 500);
-      },
-      function(error){
-        console.log(error);
-      }
-    );
-    
-    blog.selectedTab = 'blog';
-    
-    blog.selectTab = function(setTab){
-      blog.selectedTab = setTab;
-    };
-    
-    blog.isTabSelected = function(checkTab){
-      return blog.selectedTab === checkTab;
-    };
-
-    blog.toggleTag = function(tag){
-      var index = blog.selectedTags.indexOf(tag);
-      if (index > -1) {
-        blog.selectedTags.splice(index, 1);
-      } else {
-        blog.selectedTags.push(tag);
-      }
-      $timeout(function(){
-        $('.scrollspy').scrollSpy();
-      });
-    };
-
-    blog.clearSelectedTags = function(){
+      var blog = this;
+      blog.title = "david.veli!la";
+      blog.date = new Date();
+      blog.posts = [];
+      blog.allTags = [];
       blog.selectedTags = [];
-    }
-
-    blog.isTagSelected = function(tag){
-      return (blog.selectedTags.indexOf(tag) > -1);
-    }
-    
-  }]);
-  
-  app.controller('CommentController', ['$http', '$cookies', function($http, $cookies){
-    this.comment = {};
-    this.addComment = function(post){
-      if (this.comment.captcha != (post.id % 8)) {
-        var $toastContent = $('<span>Check your maths!</span>');
-        Materialize.toast($toastContent, 5000);
-        return false;
-      }
-      this.comment.createdOn = Date.now();
-      post.comments.push(this.comment);
 
       $http({
-        method: 'POST',
-        url: 'http://localhost:8000/blog/posts/' + post.id + '/comment',
-        headers: {
-         'Content-Type': 'application/json'
-        }, 
-        data: JSON.stringify({
-          author: this.comment.author,
-          body: this.comment.body,
-          captcha: this.comment.captcha
-        })
+        method: 'GET',
+        url: 'http://localhost:8000/blog/posts'
       }).then(
-        function(data){
-          var $toastContent = $('<span>Success posting comment</span>');
-          Materialize.toast($toastContent, 5000);
+        function(response){
+          $http.defaults.headers.post['X-CSRFToken'] = $cookies.get('csrftoken');
+
+          allPostsData = response.data;
+          for (var i = 0; i < allPostsData.length; i++) {
+            var postData = allPostsData[i];
+            var post = new Post(postData);
+            for (var j = 0; j < post.tags.length; j++) {
+              var tag = post.tags[j];
+              if (-1 == blog.allTags.indexOf(tag)) {
+                blog.allTags.push(tag);
+              }
+            }
+            blog.posts.push(post);
+          }
+          blog.posts[0].toggle();
+          $timeout(onDomLoaded, 500);
         },
         function(error){
-          var $toastContent = $('<span>Error posting comment</span>');
-          Materialize.toast($toastContent, 5000);
+          console.log(error);
         }
       );
+      
+      blog.selectedTab = 'blog';
+      
+      blog.selectTab = function(setTab){
+        blog.selectedTab = setTab;
+      };
+      
+      blog.isTabSelected = function(checkTab){
+        return blog.selectedTab === checkTab;
+      };
+
+      blog.toggleTag = function(tag){
+        var index = blog.selectedTags.indexOf(tag);
+        if (index > -1) {
+          blog.selectedTags.splice(index, 1);
+        } else {
+          blog.selectedTags.push(tag);
+        }
+        $timeout(function(){
+          $('.scrollspy').scrollSpy();
+        });
+      };
+
+      blog.clearSelectedTags = function(){
+        blog.selectedTags = [];
+      }
+
+      blog.isTagSelected = function(tag){
+        return (blog.selectedTags.indexOf(tag) > -1);
+      }
+
+  }]);
+  
+  app.controller('CommentController', [
+            '$scope', '$http', '$cookies', 
+    function($scope,   $http,   $cookies) {
+    
       this.comment = {};
-    }
+      this.addComment = function(post){
+        if (this.comment.captcha != (post.id % 8)) {
+          Materialize.toast('Check your math!', 5000);
+          return false;
+        }
+        this.comment.createdOn = Date.now();
+        post.comments.push(this.comment);
+
+        $http({
+          method: 'POST',
+          url: 'http://localhost:8000/blog/posts/' + post.id + '/comment',
+          headers: {
+           'Content-Type': 'application/json'
+          }, 
+          data: JSON.stringify({
+            author: this.comment.author,
+            body: this.comment.body,
+            captcha: this.comment.captcha
+          })
+        }).then(
+          function(data){
+            Materialize.toast('Thanks for your comment!', 5000);
+            $scope.commentForm.$setPristine(true)
+          },
+          function(error){
+            Materialize.toast('Error commenting', 5000);
+          }
+        );
+        this.comment = {};
+      }
+      
   }]);
 
   function onDomLoaded() {
